2bb0dd3bb3abd33ca1447c896f267a4f30d912c9
Mo-Greene <13blueboy13@daum.net>
2023-06-02 13:07:32 +0900

    feat: security 적용 시작

---
+++ backend/src/main/java/com/mogreene/backend/controller/LoginController.java
@@ -0,0 +1,13 @@
+package com.mogreene.backend.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 로그인 컨트롤러
+ */
+@Slf4j
+@RestController
+public class LoginController {
+
+}
